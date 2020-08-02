59e1f74b41c797e64ef8fdc4bd3b5ffa8a1cbe83
Sean <sweickel@gmail.com>
2020-08-01 22:33:36 -0400

    checks for winner, starting logic on cpuTurn

--- scripts/main.js
+++ scripts/main.js
@@ -20,7 +20,36 @@ function gameCtlr() {
   let playerTwo = "";
   let arrOne = [];
   let arrTwo = [];
-  let currentTurn = 0;
+  let currentTurn = 1;
+
+  function randomNum() {
+    return Math.floor(Math.random() * 9);
+  }
+
+  function cpuTurnEasy() {
+    let b = [];
+    let c = randomNum();
+    console.log("computer turn " + c);
+
+    for (let i = 0; i < gameArr.length; i++) {
+      if (gameArr[i] === "") {
+        b.push(i);
+      }
+    }
+
+    function inArray() {
+      if (gameArr[c] != "") {
+        gameArr[c] = "o";
+      } else if (gameArr[c] === "") {
+        c = randomNum();
+        inArray();
+      }
+    }
+    inArray();
+    b = [];
+    render();
+    filter();
+  }
 
   function createPlayer() {
     playerOne = prompt("Please enter your name.", "Player One");
@@ -41,7 +70,8 @@ function gameCtlr() {
     if (currentPlayer === "one") {
       setCur("two");
       domId("one").classList.remove("current");
-    } else {
+      cpuTurnEasy();
+    } else if (currentPlayer === "two") {
       setCur("one");
       domId("two").classList.remove("current");
     }
@@ -58,36 +88,52 @@ function gameCtlr() {
       }
     }
     checkWinner();
-    console.log(arrOne);
-    console.log(arrTwo);
+    console.log("current turn " + currentTurn);
   }
 
   function checkWinner() {
     let checker = (arr, target) => target.every((v) => arr.includes(v));
-    for (let i = 0; i < winningCombos.length; i++) {
-      if (checker(arrOne, winningCombos[i])) {
-        console.log("play 1 wins");
+    console.log(gameArr);
+    if (currentPlayer === "one") {
+      for (let i = 0; i < winningCombos.length; i++) {
+        if (checker(arrOne, winningCombos[i])) {
+          console.log("play 1 wins");
+        } else if (!checker(arrOne, winningCombos[i]) && currentTurn === 10) {
+          console.log("it's a draw");
+        }
       }
-    }
-
-    for (let i = 0; i < winningCombos.length; i++) {
-      if (checker(arrTwo, winningCombos[i])) {
-        console.log("play 2 wins");
+    } else if (currentPlayer === "two") {
+      for (let i = 0; i < winningCombos.length; i++) {
+        if (checker(arrTwo, winningCombos[i])) {
+          console.log("play 2 wins");
+        }
       }
-    }
-
-    if (currentTurn === 9) {
-      console.log("it's a draw");
     }
     nextTurn();
   }
 
   //renders to DOM
+  /*function render() {
+    clearBoard();
+    gameArr.forEach((element, index) => {
+      let wrapper = document.createElement("p");
+      wrapper.classList.add("marker");
+      wrapper.textContent = gameArr[index];
+      wrapper.addEventListener("click", (event) => {
+        if (wrapper.textContent === "") {
+          gameArr[index] = "x";
+        }
+      });
+      gameBrd.appendChild(wrapper);
+    });
+    render();
+    filter();
+  }*/
+
   function render() {
     clearBoard();
     gameArr.forEach((element, index) => {
       let wrapper = document.createElement("p");
-
       wrapper.classList.add("marker");
       wrapper.textContent = gameArr[index];
       wrapper.addEventListener("click", (event) => {
@@ -98,7 +144,6 @@ function gameCtlr() {
         }
         render();
         currentTurn++;
-        console.log(gameArr);
         filter();
         //nextTurn();
       });
@@ -110,7 +155,6 @@ function gameCtlr() {
   function clearBoard() {
     //remove all nodes
     const brdLength = gameBrd.children.length;
-
     for (let i = brdLength - 1; i >= 0; i--) {
       gameBrd.removeChild(gameBrd.children[i]);
     }
@@ -118,6 +162,7 @@ function gameCtlr() {
 
   document.getElementById("new").addEventListener("click", (event) => {
     gameArr = ["", "", "", "", "", "", "", "", ""];
+    currentTurn = 1;
     clearBoard();
     render();
     createPlayer();
